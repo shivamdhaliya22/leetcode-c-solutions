74f51a79703ceb3372d15aa399bc4d3a55036039
Shivam Dhaliya <shivam.dhaliya.cse.2025@miet.ac.in>
2026-08-05 16:15:33 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (56.00%) - LeetSync

---
+++ 2048-build-array-from-permutation/build-array-from-permutation.c
@@ -0,0 +1,15 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* buildArray(int* nums, int numsSize, int* returnSize) {
+    *returnSize = numsSize;
+
+    int *result = (int *)malloc(numsSize * sizeof(int));
+
+    for (int i = 0; i < numsSize; i++) {
+        result[i] = nums[nums[i]];
+    }
+
+    return result;
+    
+}
