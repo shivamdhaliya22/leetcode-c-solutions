2ec18705b7dd9b09bce5b820b5dc92b849c88f80
Shivam Dhaliya <shivam.dhaliya.cse.2025@miet.ac.in>
2026-08-15 19:41:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (81.55%) - LeetSync

--- 268-missing-number/missing-number.c
+++ 268-missing-number/missing-number.c
@@ -1,15 +1,23 @@
 int missingNumber(int* nums, int numsSize) {
 
     // sum of nums from 0 to n
-    int expSum = (numsSize * (numsSize + 1)) / 2;
+//    int expSum = (numsSize * (numsSize + 1))/2;
 
     // sum of elements in the array
-    int actualSum = 0;
-    for (int i=0; i<numsSize; i++) {
-        actualSum += nums[i];
-    }
+//    int actualSum = 0;
+//    for (int i=0; i<numsSize; i++) {
+//        actualSum += nums[i];
+//    }
 
     // missing num = difference 
-    return expSum - actualSum;
+//    return expSum - actualSum;
 
+
+    // ****by XOR or Bit Manipulaton method****
+    int xorSum = numsSize;
+
+    for (int i=0; i<numsSize; i++) {
+        xorSum ^= i ^ nums[i];
+    }
+    return xorSum;
 }
