0ec227188b620b432412663a86ddf447d84712b4
Shivam Dhaliya <shivam.dhaliya.cse.2025@miet.ac.in>
2026-09-16 18:06:32 +0530

    Time: 193 ms (10.44%) | Memory: 14.5 MB (91.55%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.c
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+    struct ListNode* mergeTwoLists(struct ListNode* l1, 
+                               struct ListNode* l2) {
+
+    struct ListNode dummy;
+    struct ListNode* tail = &dummy;
+
+    dummy.next = NULL;
+
+    while (l1 != NULL && l2 != NULL) {
+
+        if (l1->val <= l2->val) {
+            tail->next = l1;
+            l1 = l1->next;
+        }
+        else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+
+        tail = tail->next;
+    }
+
+    // Add remaining nodes
+    if (l1 != NULL)
+        tail->next = l1;
+    else
+        tail->next = l2;
+
+    return dummy.next;
+}
+
+struct ListNode* mergeKLists(struct ListNode** lists, int listsSize) {
+    
+     struct ListNode* result = NULL;
+
+    for (int i = 0; i < listsSize; i++) {
+        result = mergeTwoLists(result, lists[i]);
+    }
+
+    return result;
+
+}
